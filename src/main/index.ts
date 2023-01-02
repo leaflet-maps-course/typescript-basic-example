505eac15043df3a32a25b52bb0f7d58d19c33788
Anartz Mugika Ledo <mugan86@gmail.com>
2023-01-02 20:30:10 +0100

    feat: define boundaries box with manual input

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { Map } from 'leaflet';
 import { startMapTemplate } from '../assets/template/content';
 import { tileLayerSelect } from '../config/functions';
@@ -27,3 +28,16 @@ boundsData({
 }).addTo(mymap);
 
 tileLayerSelect().addTo(mymap);
+
+// Define boundaries box with manual limits use control reference
+const queryOverPass = `
+[bbox:43.13669397035837,-2.5096893310546875,43.21180708932604,-2.316055297851563]
+[out:json][timeout:25];( 
+node["amenity"="drinking_water"]; 
+node["natural"="spring"]; 
+node["drinking_water"="yes"]; 
+);out center;>;`
+
+axios
+  .post("https://overpass-api.de/api/interpreter", queryOverPass)
+  .then(({ data }) => console.log(data));
