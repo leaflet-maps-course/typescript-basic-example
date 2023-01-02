16c45e8bf0ef7763e249811aa30925e4fdcea601
Anartz Mugika Ledo <mugan86@gmail.com>
2023-01-02 20:11:54 +0100

    feat: project ok to start tutorial

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,26 +1,32 @@
-import { Map } from 'leaflet';
+import { latLngBounds, Map } from 'leaflet';
 import { startMapTemplate } from '../assets/template/content';
 import { tileLayerSelect } from '../config/functions';
 import { boundsData } from './bounds-control';
 
-// https://stackblitz.com/edit/angular-ivy-6e5zvw?file=src/app/app.component.ts
 
 startMapTemplate(document, 'Plantilla - Mapa con Typescript');
 
 const mymap = new Map('map').setView([43.3082977, -1.9837398], 10);
 
+// Data to add in control
 const mapBounds = {
     northEast: {
-        lat: mymap.getBounds().getNorthEast().lat,
-        lng: mymap.getBounds().getNorthEast().lng
+        lat: 43.21180708932604,
+        lng: -2.340774536132813
     },
     southWest: {
-        lat: mymap.getBounds().getSouthWest().lat,
-        lng: mymap.getBounds().getSouthWest().lng
+        lat: 43.13669397035837,
+        lng: -2.484970092773438
     },
 };
+
+// Use data to center camera with correct zoom and position
+const fitBoundsData = latLngBounds(mapBounds.southWest, mapBounds.northEast)
+
 boundsData({
     bounds: mapBounds
 }).addTo(mymap);
+
+mymap.fitBounds(fitBoundsData);
 
 tileLayerSelect().addTo(mymap);
